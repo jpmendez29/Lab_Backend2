dd2607fce018772d862e1ed6dd3b38f40e3ea80a
jpmendez29 <juan.pablomendez@hotmail.com>
2022-11-24 18:23:49 -0500

    Creacion index y carpetas

---
+++ app.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import userRoutes from './user/user.route.js'; // falta usuarios
+require('dotenv').config();
+const [user,pasw] = [process.env.USER, process.env.PASW]
+
+
+export function TestApp() {
+    // MIDDLEWARE JSON
+    const app = express();
+
+    app.use(express.json())
+
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', '*');
+
+        if (req.method === 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
+        next();
+    });
+
+
+    /* // DECLARACION DE RUTA
+    const routeUs = require('./Rutas/Rutas_Usuarios')
+    const routePub = require('./Rutas/Rutas_Publicaciones')
+    const routeSeg = require('./Rutas/Rutas_Seguimientos')
+    const routeLikes = require('./Rutas/Rutas_Likes')
+    app.use("/Usuarios", routeUs)
+    app.use("/Publicaciones", routePub)
+    app.use("/Seguimientos", routeSeg)
+    app.use("/Likes", routeLikes) */
+    
+    app.use('/users', userRoutes)
+
+    app.use(async (req, res) => {
+        res.status(404).json({message: "Not found."})
+    });
+
+    return app
+}
+
+
+export function App() {
+    const app = TestApp()
+
+    // CONEXION MONGO
+    mongoose.connect(
+        "mongodb+srv://"+user+":"+pasw+"pn@labbackend.wsxmbzf.mongodb.net/Twitter?retryWrites=true&w=majority"
+        )
+    .then(() => {
+        console.log("Conectado a la base de datos");
+    })
+    .catch((e) => {
+        console.log(e)
+        console.log("Error de conexion")
+    })
+
+    return app
+}
--- index.js
+++ index.js
@@ -1,40 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-/*const [user,pasw] = [process.env.USER, process.env.PASW]
+import http from 'http';
+import {App} from './app'
+const app = App()
+const server = http.createServer(app);
 
-
-// MIDDLEWARE JSON
-app.use(express.json());
-
-// CONEXION MONGO
-mongoose.connect(
-    "mongodb+srv://"+user+":"+pasw+"pn@labbackend.wsxmbzf.mongodb.net/Twitter?retryWrites=true&w=majority"
-    )
-.then(() => {
-    console.log("Conectado a la base de datos");
-})
-.catch((e) => {
-    console.log(e)
-    console.log("Error de conexion")
-})*/
-
-// DECLARACION DE RUTA
-const routeUs = require('./Rutas/Rutas_Usuarios')
-const routePub = require('./Rutas/Rutas_Publicaciones')
-const routeSeg = require('./Rutas/Rutas_Seguimientos')
-const routeLikes = require('./Rutas/Rutas_Likes')
-app.use("/Usuarios", routeUs)
-app.use("/Publicaciones", routePub)
-app.use("/Seguimientos", routeSeg)
-app.use("/Likes", routeLikes)
 
 // ABRIR PUERTO PARA APP
-app.listen(3000, (err) => {
+server.listen(3000, (err) => {
     if(err){
         console.log("Error", err)
         return
     }
-    console.log("listening on port 3000")
+    console.log("Server listening on port 3000")
 });
+
