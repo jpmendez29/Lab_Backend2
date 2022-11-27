7da3cffcc65a93f975b018c92cc23ae99cb9c65d
jpmendez29 <juan.pablomendez@hotmail.com>
2022-11-27 02:05:47 -0500

    Modelos actualizados

--- Reseñas/Reseñas.model.js
+++ Reseñas/Reseñas.model.js
@@ -4,8 +4,8 @@ const ReseñaSchema = new mongoose.Schema(
     {
     Reseña: {type: String, trim: true, required: true},
     Puntuacion : {type: Number, trim: true, required: true},
-    id_Usuario: {type: mongoose.Types.ObjectId, trim: true, required: true},
-    id_Producto: {type: mongoose.Types.ObjectId, trim: true, required: true},
+    Id_Usuario: {type: mongoose.Types.ObjectId, trim: true, required: true},
+    Id_Producto: {type: mongoose.Types.ObjectId, trim: true, required: true},
     },
     {
         timestamps: true,
