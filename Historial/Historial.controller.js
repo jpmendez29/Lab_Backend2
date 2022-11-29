4bb84fb3cd3117d08ad3b447132ffb99e3b0dcf0
jpmendez29 <juan.pablomendez@hotmail.com>
2022-11-29 03:12:25 -0500

    debug Historial

--- Historial/Historial.controller.js
+++ Historial/Historial.controller.js
@@ -6,11 +6,9 @@ import HistModel from "../Historial/Historial.model.js"
 export async function GetHist(req) {
     const token = req.headers.authorization.split(' ').pop()
     const tokendata = await Verifytoken(token)
-    const Hist = await HistModel.find({Id_Usuario: tokendata._id})
     const Res = await HistModel.aggregate([
         {$match: {"Id_Usuario": mongoose.Types.ObjectId(tokendata._id)}},
         {$project: {Historial: { _id: 0, Id_Usuario: 0, Id_Usuario:0, createdAt: 0 , updatedAt:0, __v:0, Productos: {_id:0, id_prod:0}}}},
-        {$unwind: "$Historial"},
         {$project: { _id: 0, Id_Usuario: 0, Id_Usuario:0, createdAt: 0 , updatedAt:0, __v:0 }},
         
     ])
